a610904cc017959ae5467f7bfe905bbee6c54f22
Teske <11568034@nwtc.edu>
2017-09-14 13:23:04 -0500

    Moved to new folder

---
+++ Clients/LPI/get-logofftime.ps1
@@ -0,0 +1,5 @@
+﻿$user = $env:USERNAME
+$date = get-date -Format d
+$time = get-date -Format t
+
+Write-Output "$user logged out on $date at $time" | out-file -FilePath \\lpierp\data\logon\logon.txt -Append
