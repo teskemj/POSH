c4f362d682b47e0b2c43a8f371f0e1db57a8cd8a
Michael Teske <michael.teske@outlook.com>
2018-01-17 09:16:29 -0600

    Testing MarkDown

--- get-api.ps1
+++ get-api.ps1
@@ -15,7 +15,53 @@ find-module *git* | fl name,description
 Find-Module *weather*
 install-module psweather
 gcm -Module psweather
-help get-weather
+help get-weather// for every command:
+# {Command name}
+
+## SYNOPSIS
+{{Synopsis text}}
+
+## DESCRIPTION
+{{Description text}}
+
+## PARAMETERS
+
+// for every parameter
+    // type and default value are non-mandatory
+    ### {Parameter name} [{Parameter type}] = {Parameter default value}
+
+    // parameter metadata
+    ```powershell
+    {{Parameter attributes as specified in param() block in PowerShell functions
+    i.e. [Parameter(ParameterSetName = 'ByName')]
+    }}
+    ```
+
+    {{Parameter description text}}
+
+## INPUTS
+// for every input type
+    #### {Input type}
+    {{Description text}}
+
+## OUTPUTS
+// for every output type
+    ### {Output type}
+    {{Description text}}
+
+## EXAMPLES
+// for every example
+    ### {Example Name}
+
+    ```powershell
+    {{Example body}}
+    ```
+    {{Example text explanation}}
+
+## RELATED LINKS
+
+// for every link
+    [{link name}]({link url})
 get-weather -days 5
 get-weather -zip 54115
 get-weather -zip 54115 -days 15
@@ -41,7 +87,7 @@ Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Met
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default | gm
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default | select *
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default | select * | gm
-Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default
+[Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/chart/1m" -Method Default
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/financials" -Method Default
 Invoke-RestMethod -uri "https://api.iextrading.com/1.0/stock/csco/financials" -Method Default | fl
