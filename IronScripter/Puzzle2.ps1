30e4672136cadca7196d2174c48827045efd70d9
Michael Teske <michael.teske@outlook.com>
2018-04-03 21:56:33 -0500

    changed font color

--- IronScripter/Puzzle2.ps1
+++ IronScripter/Puzzle2.ps1
@@ -1,13 +1,13 @@
 $objOperatingSystem = Get-WmiObject -Query "SELECT * FROM Win32_OperatingSystem" 
-Write-Host "OS Name: " $objOperatingSystem.Name 
+Write-Host -ForegroundColor green "OS Name: " $objOperatingSystem.Name 
 Write-Host "Version: " $objOperatingSystem.Version 
 Write-Host "Service Pack: " $objOperatingSystem.ServicePackMajorVersion "." $objOperatingSystem.ServicePackMinorVersion 
 Write-Host "OS Manufacturer: " $objOperatingSystem.Manufacturer
 Write-Host "Windows Directory: " $objOperatingSystem.WindowsDirectory 
 Write-Host "Locale: " $objOperatingSystem.Locale 
-Write-Host "Available Physical Memory: " $objOperatingSystem.FreePhysicalMemory 
-Write-Host "Total Virtual Memory: " $objOperatingSystem.TotalVirtualMemorySize 
-Write-Host "Available Virtual Memory: " $objOperatingSystem.FreeVirtualMemory 
+Write-Host "Available Physical Memory: " $objOperatingSystem.FreePhysicalMemory
+Write-Host "Total Virtual Memory: " $objOperatingSystem.TotalVirtualMemorySize
+Write-Host "Available Virtual Memory: " $objOperatingSystem.FreeVirtualMemory
 $disks = Get-WmiObject -Query "SELECT * FROM Win32_LogicalDisk" 
 foreach ($objDisk in $disks) { 
     Write-Host "Drive: " $objDisk.DeviceID 
