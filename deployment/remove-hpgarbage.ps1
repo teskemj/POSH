fa241549e81316645f6255ae10499738b90cc935
teskemj <michael.teske@outlook.com>
2018-12-14 16:44:14 -0600

    Cleaned up foreach and added an explicit remove of an app

--- deployment/remove-hpgarbage.ps1
+++ deployment/remove-hpgarbage.ps1
@@ -4,26 +4,29 @@
 .DESCRIPTION
 .NOTES
 Author: Michael TEske
-  Created: 2018-12-14
-  Modified: 2018-12-14
+Created: 2018-12-14
+Modified: 2018-12-14
 
 #http://lifeofageekadmin.com/how-to-uninstall-programs-using-powershell/
+#>
 Function Remove-HP {
     
-        $apps = Get-WmiObject -Class Win32_Product | Where-Object {
-            $_.Name -like “HP*”}
+    # Other packages are dependent on HP Device Support
+    uninstall-package -Name "HP Device Support" -Force
+    
+    # Retrieve all HP packages
+    $apps = get-package -name HP*
 
-        foreach ($app in $apps) {
+    # Process each app that isn't the HP Support Assistant. HPSA is convenient for updating drivers
+    foreach ($app in $apps) {
         if ($app.name -notlike "HP Support Assistant") {
             $app.name
             Uninstall-Package $app.name -Force
-
-            
-        }
+        } #End If
+    } #End foreach
         
     }
 
-}
-$apps = get-package -name HP*
+} #End Function
 
 Install-BoxstarterPackage -PackageName http://gist.github.com/teskemj/deployhp.ps1 -DisableReboots
