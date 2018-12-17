9fa6ae4762e2f725a724c6a6825cc04a676e5127
teskemj <michael.teske@outlook.com>
2018-12-17 15:32:54 -0600

    Removed unnecessary comment

--- deployment/remove-hpgarbage.ps1
+++ deployment/remove-hpgarbage.ps1
@@ -56,7 +56,7 @@ rename-computer -NewName ksi-122018-5 -restart
 
 ## Start here after rebooot ##
 
-# Side note This installs boxstarter. Once installed. Insert the "raw" GIST link
+# Side note This installs boxstarter. Once installed. 
 # place cursor on the following line and press F8
 
 . { Invoke-WebRequest -useb http://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; get-boxstarter -Force
